ee748e48e4c80a950973cc1c417f46f85dce2619
Himsself1 <spapadadonakis@gmail.com>
2024-09-26 01:58:07 +0300

    Added ancestral populations.

--- msprime_scripts/msprime_no_migration.py
+++ msprime_scripts/msprime_no_migration.py
@@ -15,6 +15,19 @@ cli = argparse.ArgumentParser(
 
 # ** Configure split times
 
+## Need to configure these split times in order to finalize the model
+
+split_time_2_3
+split_time_1_2_3
+split_time_0_1_2_3
+split_time_5_6
+split_time_7_5_6
+split_time_8_7_5_6
+split_time_ingroups
+split_time_ingroups_out_2
+split_time_ancestral_all
+
+
 demography = msprime.Demography()
 
 # ** Create populations 0-9
@@ -33,13 +46,64 @@ demography.add_population(name = "ancestral_0_1_2_3")
 demography.add_population(name = "ancestral_5_6")
 demography.add_population(name = "ancestral_7_5_6")
 demography.add_population(name = "ancestral_8_7_5_6")
+demography.add_population(name = "ancestral_ingroups")
+demography.add_population(name = "ancestral_all")
+
 
 # ** Create coalescent events
 
 demography.add_population_split(
     time = split_time_2_3,
     derived = [ "pop_2", "pop_3"],
-    ancestral=ancestral = "ancestral_2_3"
+    ancestral = "ancestral_2_3"
+)
+
+demography.add_population_split(
+    time = split_time_1_2_3,
+    derived = [ "ancestral_2_3", "pop_1"],
+    ancestral = "ancestral_1_2_3"
+)
+
+demography.add_population_split(
+    time = split_time_0_1_2_3,
+    derived = [ "ancestral_1_2_3", "pop_0"],
+    ancestral = "ancestral_0_1_2_3"
+)
+
+demography.add_population_split(
+    time = split_time_5_6,
+    derived = [ "pop_5", "pop_6"],
+    ancestral = "ancestral_5_6"
+)
+
+demography.add_population_split(
+    time = split_time_7_5_6,
+    derived = [ "ancestral_5_6", "pop_7"],
+    ancestral = "ancestral_7_5_6"
+)
+
+demography.add_population_split(
+    time = split_time_8_7_5_6,
+    derived = [ "ancestral_7_5_6", "pop_8"],
+    ancestral = "ancestral_8_7_5_6"
+)
+
+demography.add_population_split(
+    time = split_time_ingroups,
+    derived = [ "ancestral_8_7_5_6", "ancestral_0_1_2_3"],
+    ancestral = "ancestral_ingroups"
+)
+
+demography.add_population_split(
+    time = split_time_ingroups_out_2,
+    derived = [ "ancestral_ingroups", "outpop_2"],
+    ancestral = "ancestral_outpop_2"
+)
+
+demography.add_population_split(
+    time = split_time_ancestral_all,
+    derived = [ "ancestral_outpop_2", "outpop_1"],
+    ancestral = "ancestral_all"
 )
 
 # ** Create the admixed population (pop_4)
