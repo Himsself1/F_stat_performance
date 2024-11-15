ca6490ccce46aaa9427d5c8e0b6975b3cf82e070
Himsself1 <spapadadonakis@gmail.com>
2024-11-15 16:01:46 +0200

    Started implementing qpadm analysis.

--- qpadm_inference/qpadm_inference.R
+++ qpadm_inference/qpadm_inference.R
@@ -1,3 +1,21 @@
+# * Libraries
+
+list_of_packages <- c(
+  "ggplot2", "devtools",
+  "argparse", "stringr",
+  "Cairo"
+)
+
+for (i in list_of_packages) {
+  if (!require(i, character.only = TRUE)) {
+    install.packages(i)
+  }
+}
+
+devtools::install_github("uqrmaie1/admixtools")
+library(admixtools)
+
+
 # * Command line arguments
 
 # * Assignment of individuals to populations
@@ -6,14 +24,15 @@
 ## Each of the outgroup populations have 10 samples.
 ## Outgroup samples are located after the ingroup samples in the vcf
 
-
-normal_pops <- rep( sapply( 1:9, function(x){
+normal_pops <- rep( sapply( 0:8, function(x){
   paste0( "pop_", x )
 }), each = 5)
 
-outgroups <- rep( sapply( 1:2, function(x){
+outgroups <- rep( sapply( 0:1, function(x){
   paste0( "outpop_", x )
 }), each = 10)
 
 
 all_names <- c(normal_pops, outgroups)
+
+target <- "pop_4"
