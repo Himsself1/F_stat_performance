1bb672e004efbf43c4ba7c58bff3aa2615cfbb93
Himsself1 <spapadadonakis@gmail.com>
2024-11-19 02:05:07 +0200

    Created an structure to hold all the models.

--- qpadm_inference/qpadm_inference.R
+++ qpadm_inference/qpadm_inference.R
@@ -8,13 +8,12 @@ list_of_packages <- c(
 
 for (i in list_of_packages) {
   if (!require(i, character.only = TRUE)) {
-    install.packages(i)
+    install.packages(i, dependencies = T)
   }
 }
 
 devtools::install_github("uqrmaie1/admixtools")
 library(admixtools)
-
 
 # * Command line arguments
 
@@ -45,25 +44,52 @@ population_names <- unique(individuals_in_populations)
 target <- "pop_4"
 ancestors_of_pop3 <- paste("pop_", c(0:3), sep = '')
 ancestors_of_pop5 <- paste("pop_", c(5:8), sep = '')
-all_models <- expand.grid(ancestors_of_pop3, target, ancestors_of_pop5, stringsAsFactors = F)
+all_models <- expand.grid(ancestors_of_pop3, ancestors_of_pop5, stringsAsFactors = F)
 
 ## Runs f2 for all populations \
 ## TODO: Make a loop for all pairs of ancestors.
 ## Should I make pairs of ancestors from the same side of the tree?
 
-f2_from_geno(
-  prefix = input_prefixes[1],
+f2_blocks_for_single_model <- f2_from_geno(
+  pref = input_prefixes[1],
   inds = individuals_in_populations
 )
 
 ## Need to make a structure that contains all the combinations of `right` populations.
 
-right_pops <- population_names[!(population_names %in% all_models[1, ])]
-list_of_right_pops <- lapply(1:(length(right_pops) - 2), FUN = function(x) {
-  right_pops[-x]
-})
-names(list_of_right_pops) <- right_pops[1:(length(right_pops)-2)]
-list_of_right_pops$all <- right_pops
+## Each index of the variables bellow will consist of a qpadm model.
+left_all <- list()
+right_all <- list()
+target_all <- list()
+for (i in nrow(all_models)) {
+  right_pops <- population_names[!(population_names %in% c(all_models[i, ], target))]
+  ## `list_of_right_pops ` will include all the permutations of right populations where only 1 is missing
+  list_of_right_pops <- lapply(1:(length(right_pops) - 2), FUN = function(x) {
+    right_pops[-x]
+  })
+  names(list_of_right_pops) <- right_pops[1:(length(right_pops) - 2)]
+  list_of_right_pops$all <- right_pops  ## Lastly, add the model where none of the populations are missing
+  for (j in 1:length(list_of_right_pops)) {
+    left_all <- c(left_all, list(all_models[i, ]))
+    right_all <- c(right_all, list(list_of_right_pops[[j]]))
+    target_all <- c(target_all, target)
+  }
+}
+
+?qpadm_multi
+    
+## right_pops <- population_names[!(population_names %in% all_models[1, ])]
+## list_of_right_pops <- lapply(1:(length(right_pops) - 2), FUN = function(x) {
+##   right_pops[-x]
+## })
+## names(list_of_right_pops) <- right_pops[1:(length(right_pops)-2)]
+## list_of_right_pops$all <- right_pops
+
+qpadm(
+  f2_blocks_for_single_model,
+  left = all_models[i, c(1, 3)],
+  right = right_pops[[i]]
+)
 
 ## Need to make a loop for qpadm for all sets of right pops given the ancestors
 
