0ce7ea3666ca0094e36ec0c10b42a579ed3878b8
Himsself1 <spapadadonakis@gmail.com>
2024-11-20 16:21:18 +0200

    Implemented qpadm scheme according to Lazaridis (2024)

--- pipeline/run_no_migration_constant_size.sh
+++ pipeline/run_no_migration_constant_size.sh
@@ -1,21 +1,22 @@
 #! /bin/bash
 
-MASTER_OUT_FOLDER="/home/stefanos/simulations/inference_estimation/"
-MODEL_FOLDER="no_migration_constant_size"
+MASTER_OUT_FOLDER="/home/stefanos/new_storage/inference_estimation/"
+MODEL_FOLDER="sequencies/no_migration_constant_size"
 VCF_FOLDER=$MASTER_OUT_FOLDER$MODEL_FOLDER"/vcf/"
 EIGENSTRAT_FOLDER=$MASTER_OUT_FOLDER$MODEL_FOLDER"/eig/"
+STATISTICS_FOLDER=$MASTER_OUT_FOLDER"statistics"
 
 mkdir -p $MASTER_OUT_FOLDER
 mkdir -p $VCF_FOLDER
 mkdir -p $EIGENSTRAT_FOLDER
-
+mkdir -p $STATISTICS_FOLDER
 
 # * msprime scripts
 
 python3 ../msprime_scripts/msprime_no_migration.py \
     -out_folder $VCF_FOLDER \
     -name no_migration_constant_size \
-    -how_many 1
+    -how_many 10
     
 ## Need to record output folders
 
--- qpadm_inference/qpadm_inference.R
+++ qpadm_inference/qpadm_inference.R
@@ -22,6 +22,14 @@ input_folder <- args[1]
 output_folder <- args[2]
 
 input_files <- list.files(path = input_folder, pattern = ".geno", full.names = TRUE)
+
+## Debug ##
+input_files <- list.files(
+  path = "/media/storage/stef_sim/inference_estimation/sequencies/no_migration_constant_size/eig",
+  pattern = ".geno", full.names = TRUE)
+###########
+
+## Need to take the full path of the prefixes to pass to f2_blocks
 input_prefixes <- gsub(pattern = ".geno", replacement = "", input_files)
 
 # * Assignment of individuals to populations 
@@ -38,33 +46,34 @@ outgroups <- rep(sapply(0:1, function(x) {
   paste0("outpop_", x)
 }), each = 10)
 
+## f2_blocks needs a vector with individual names and another vector
+## of equal size that specifies the population of each individual.
 individuals_in_populations <- c(normal_pops, outgroups)
 population_names <- unique(individuals_in_populations)
+individual_names <- paste("tsk_", 0:64, "indv", sep = '')
 
 target <- "pop_4"
-ancestors_of_pop3 <- paste("pop_", c(0:3), sep = '')
-ancestors_of_pop5 <- paste("pop_", c(5:8), sep = '')
-all_models <- expand.grid(ancestors_of_pop3, ancestors_of_pop5, stringsAsFactors = F)
+all_ancestors <- paste("pop_", c(0:3,5:8), sep = '')
 
-## Runs f2 for all populations \
-## TODO: Make a loop for all pairs of ancestors.
-## Should I make pairs of ancestors from the same side of the tree?
+## Following lines take all unique pairs of relatives.
+## E.G. combination (pop_0, pop_1) exists but not (pop_1, pop_0)
+all_models <- c()
+for( i in 1:(length(all_ancestors)-1) ){
+  for( j in (i+1):length(all_ancestors) ){
+    all_models <- rbind( all_models, c(all_ancestors[i], all_ancestors[j]) )
+  }
+}
 
-f2_blocks_for_single_model <- f2_from_geno(
-  pref = input_prefixes[1],
-  inds = individuals_in_populations
-)
-
-## Need to make a structure that contains all the combinations of `right` populations.
-
-## Each index of the variables bellow will consist of a qpadm model.
+## Need to make a structure that contains all the combinations of `right` populations,
+## as per Lazaridis (2024) scheme, i.e. dropping one `right` population each time.
+## Combining each index of the variables bellow will consist of a qpadm model.
 left_all <- list()
 right_all <- list()
 target_all <- list()
-
+exclude <- c()
 for (i in 1:nrow(all_models)) {
   right_pops <- population_names[!(population_names %in% c(all_models[i, ], target))]
-  ## `list_of_right_pops ` will include all the permutations of right populations where only 1 is missing
+  ## `list_of_right_pops`  will include all the permutations of right populations where only 1 is missing
   list_of_right_pops <- lapply(1:(length(right_pops) - 2), FUN = function(x) {
     right_pops[-x]
   })
@@ -74,6 +83,7 @@ for (i in 1:nrow(all_models)) {
     left_all <- c(left_all, list(all_models[i, ]))
     right_all <- c(right_all, list(list_of_right_pops[[j]]))
     target_all <- c(target_all, target)
+    exclude <- c(exclude, names(list_of_right_pops)[[j]])
   }
 }
 
@@ -83,25 +93,26 @@ qp_models <- tibble(
   target = target_all
 )
 
-?tibble
+## Need to loop the following for all replicates
 
-## right_pops <- population_names[!(population_names %in% all_models[1, ])]
-## list_of_right_pops <- lapply(1:(length(right_pops) - 2), FUN = function(x) {
-##   right_pops[-x]
-## })
-## names(list_of_right_pops) <- right_pops[1:(length(right_pops)-2)]
-## list_of_right_pops$all <- right_pops
+f2_blocks_for_single_model <- f2_from_geno(
+  pref = input_prefixes[1],
+  pops = individuals_in_populations,
+  inds = individual_names
+)
+f2_blocks_for_single_model
 
-qpadm(
-  f2_blocks_for_single_model,
-  left = all_models[i, c(1, 3)],
-  right = right_pops[[i]]
+## qpadm_multi iterates over a tibble whose rows represent
+## right and left populations and target
+qpadm_multi(
+  data = f2_blocks_for_single_model,
+  models = qp_models
 )
 
-## Need to make a loop for qpadm for all sets of right pops given the ancestors
 
 ### Following steps
-## Make Lazaridis scheme
-## Run qpadm
+## Make Lazaridis scheme DONE
+## Run qpadm DONE
 ## Implement scoring function
+## Loop previous 3
 ## Plot results
